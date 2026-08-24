eea818d27977cd9374b247d4a86b33ae7070575e
Fain <25233821+jcfain@users.noreply.github.com>
2026-08-24 15:44:53 -0400

    Add G0 command

--- Providers/TCodeProvider.cs
+++ Providers/TCodeProvider.cs
@@ -553,7 +553,7 @@ public class TCodeProvider : ProviderBase
         {
             intervalOut ="I" + speed.ToString();
         }
-        string ret = channel.Name + value.ToString().PadLeft(4, '0') + intervalOut;
+        string ret = channel.Name + value.ToString().PadLeft(4, '0') + intervalOut + "G0";
         
         //channel.Unlock();
         return ret;
